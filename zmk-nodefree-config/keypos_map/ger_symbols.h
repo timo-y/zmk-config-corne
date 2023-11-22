d0e79158539c00f3c640d2a0f730fab91f0fe838
Timo Yu <Timo.Yu@pta.de>
2023-11-22 09:56:51 +0100

    german keymap extended

--- zmk-nodefree-config/keypos_map/ger_symbols.h
+++ zmk-nodefree-config/keypos_map/ger_symbols.h
@@ -1,37 +1,83 @@
-/* symbols */
+/* german keymap */
 #pragma once
 
-#define DE_USCORE        QMARK
-#define DE_CIRCLE        TILDE
-#define DE_LBRCKT        RA(N8)
-#define DE_RBRCKT        RA(N9)
-#define DE_LBRACE        RA(N7)
-#define DE_RBRACE        RA(N0)
-#define DE_LPAR          STAR
-#define DE_RPAR          LPAR
-#define DE_BSLASH        RA(MINUS)
-#define DE_FSLASH        AMPERSAND
-#define DE_CARET         GRAVE
-#define DE_STAR          RIGHT_BRACE
-#define DE_HASH          BACKSLASH
-#define DE_DOLLAR        DOLLAR
-#define DE_PIPE          RA(LESS_THAN)
-#define DE_TILDE         LS(DE_PLUS)
-#define DE_GRAVE         PLUS
-#define DE_EXCL          EXCLAMATION
-#define DE_LT            NUBS
-#define DE_GT            PIPE2
-#define DE_EQ            KP_EQUAL
-#define DE_AMP           SLASH
-#define DE_PARA          HASH
-#define DE_QMARK         UNDERSCORE
-#define DE_MINUS         KP_MINUS
-#define DE_COLON         GREATER_THAN
-#define DE_AT            RA(Q)
-#define DE_PLUS          RBRC
-#define DE_PERCENT       PERCENT
-#define DE_DQT           AT_SIGN
-#define DE_SQT           PIPE
-#define DE_SCOLON        LESS_THAN
-#define DE_EURO          RA(E)
-#define DE_GRAVE         PLUS
+// Aliases
+#define DE_CARET GRAVE  	// ^ (dead)
+#define DE_1    N1        	// 1
+#define DE_2    N2        	// 2
+#define DE_3    N3        	// 3
+#define DE_4    N4        	// 4
+#define DE_5    N5        	// 5
+#define DE_6    N6        	// 6
+#define DE_7    N7        	// 7
+#define DE_8    N8        	// 8
+#define DE_9    N9        	// 9
+#define DE_0    N0        	// 0
+#define DE_SS   MINUS		// ß
+#define DE_ACUT EQUAL	  	// ´ (dead)
+#define DE_Q    Q    		// Q
+#define DE_W    W    		// W
+#define DE_E    E    		// E
+#define DE_R    R    		// R
+#define DE_T    T   	 	// T
+#define DE_Z    Y    		// Z
+#define DE_U    U    		// U
+#define DE_I    I    		// I
+#define DE_O    O    		// O
+#define DE_P    P    		// P
+#define DE_UE	LBKT	 	// Ü
+#define DE_PLUS RBRC 		// +
+#define DE_A    A    		// A
+#define DE_S    S    		// S
+#define DE_D    D    		// D
+#define DE_F    F    		// F
+#define DE_G    G    		// G
+#define DE_H    H    		// H
+#define DE_J    J    		// J
+#define DE_K    K    		// K
+#define DE_L    L    		// L
+#define DE_OE	SEMI	 	// Ö
+#define DE_AE	SQT		 	// Ä
+#define DE_HASH BACKSLASH 	// #
+#define DE_LT	NUBS 		// <
+#define DE_Y    Z    		// Y
+#define DE_X    X    		// X
+#define DE_C    C    		// C
+#define DE_V    V    		// V
+#define DE_B    B    		// B
+#define DE_N    N    		// N
+#define DE_M    M    		// M
+#define DE_COMMA COMMA	 	// ,
+#define DE_PERIOD PERIOD	// .
+#define DE_MINUS SLASH 		// -
+#define DE_DEG  LS(DE_CARET)// °
+#define DE_EXLM LS(DE_1)    // !
+#define DE_DQUO LS(DE_2)    // "
+#define DE_SECT LS(DE_3)    // §
+#define DE_DLR  LS(DE_4)    // $
+#define DE_PERC LS(DE_5)    // %
+#define DE_AMPR LS(DE_6)    // &
+#define DE_SLSH LS(DE_7)    // /
+#define DE_LPRN LS(DE_8)    // (
+#define DE_RPRN LS(DE_9)    // )
+#define DE_EQL  LS(DE_0)    // =
+#define DE_QUES LS(DE_SS)   // ?
+#define DE_GRV  LS(DE_ACUT) // ` (dead)
+#define DE_ASTR LS(DE_PLUS) // *
+#define DE_QUOT LS(DE_HASH) // '
+#define DE_GT	LS(DE_LT)	// >
+#define DE_SCLN LS(DE_COMM) // ;
+#define DE_COLN LS(DE_DOT)  // :
+#define DE_UNDS LS(DE_MINUS)// _
+#define DE_SUP2 LA(DE_2)    // ²
+#define DE_SUP3 LA(DE_3)    // ³
+#define DE_LCBR LA(DE_7)    // {
+#define DE_LBRC LA(DE_8)    // [
+#define DE_RBRC LA(DE_9)    // ]
+#define DE_RCBR LA(DE_0)    // }
+#define DE_BSLS LA(DE_SS)   // (backslash)
+#define DE_AT   LA(DE_Q)    // @
+#define DE_EURO LA(DE_E)    // €
+#define DE_TILD LA(DE_PLUS) // ~
+#define DE_PIPE LA(DE_LABK) // |
+#define DE_MICR LA(DE_M)    // µ
